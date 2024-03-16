e504e94aa3b0dcc6c35772dac61541956f3a9e3c
Aliyavar <aliyavarguluzada@gmail.com>
2024-03-16 22:54:12 +0400

    LeftRotation HackerRank

---
+++ Algoritm_Practice/HackerRank/LeftRotation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm_Practice.HackerRank
+{
+    public static class LeftRotation
+    {
+        public static List<int> rotateLeft(int d, List<int> arr)
+        {
+            int n = arr.Count;
+
+            int[] newArray = new int[n];
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                int newIndex;
+
+                if (i - d < 0)
+                    newIndex = i - d + n;
+                else
+                    newIndex = i - d;
+
+                newArray[newIndex] = arr[i];
+
+            }
+            List<int> aa = newArray.ToList();
+
+            return aa;
+        }
+    }
+}
--- Algoritm_Practice/Program.cs
+++ Algoritm_Practice/Program.cs
@@ -1,11 +1,8 @@
-﻿
+﻿using Algoritm_Practice.HackerRank;
 
+List<int> ints = [1, 2, 3, 4, 5];
 
-
-
-
-
-
+LeftRotation.rotateLeft(2, ints);
 
 
 
