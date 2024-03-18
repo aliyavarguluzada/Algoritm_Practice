f4c9c6e6f92db2085d2a1b3bb30e66aa03a3600d
Aliyavar <aliyavarguluzada@gmail.com>
2024-03-18 21:51:43 +0400

    ContainsDuplicate Leetcode

---
+++ Algoritm_Practice/Leetcode/ContainsDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm_Practice.Leetcode
+{
+    public static class ContainsDuplicate
+    {
+        public static bool containsDuplicate(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- Algoritm_Practice/Program.cs
+++ Algoritm_Practice/Program.cs
@@ -1,17 +1,38 @@
-﻿using Algoritm_Practice.HackerRank;
+﻿
+//bool IsPrime(int number)
+//{
+//    // If the number is less than 2, it's not prime
+//    if (number < 2)
+//        return false;
 
-List<int> ints = [1, 2, 3, 4, 5];
+//    // Check divisibility from 2 to the square root of the number
+//    for (int i = 2; i <= Math.Sqrt(number); i++)
+//    {
+//        if (number % i == 0)
+//            return false; // Number is divisible, not prime
+//    }
 
-LeftRotation.rotateLeft(2, ints);
+//    return true; // If no divisor found, number is prime
+//}
 
+//Console.WriteLine(IsPrime(11));
+bool IsPrime(int number)
+{
+    // If the number is less than 2, it's not prime
+    if (number < 2)
+        return false;
 
+    // Check divisibility from 2 to the square root of the number
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+            return false; // Number is divisible, not prime
+    }
 
+    return true; // If no divisor found, number is prime
+}
 
-
-
-
-
-
+Console.WriteLine(IsPrime(11));
 
 
 
