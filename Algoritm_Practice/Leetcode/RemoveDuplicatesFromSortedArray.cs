298efae1cb53bafe5680bbe9c4a0c6c4c38e380d
Aliyavar <aliyavarguluzada@gmail.com>
2024-04-18 23:47:14 +0400

    /../

--- Algoritm_Practice/Leetcode/RemoveDuplicatesFromSortedArray.cs
+++ Algoritm_Practice/Leetcode/RemoveDuplicatesFromSortedArray.cs
@@ -13,8 +13,6 @@ namespace Algoritm_Practice.Leetcode
             // Input: nums = [0,0,1,1,1,2,2,3,3,4]
             // Output: 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
 
-            int[] ints = new int[nums.Length];
-
             int k = 0;
             HashSet<int> uniques = nums.ToHashSet();
             int[] a = uniques.ToArray();
