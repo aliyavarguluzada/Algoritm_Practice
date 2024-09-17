dcd5d81553534dff6e5137a25d20f812680bc37a
Aliyavar <aliyavarguluzada@gmail.com>
2024-09-17 22:15:02 +0400

    ./

--- Algoritm_Practice/Program.cs
+++ Algoritm_Practice/Program.cs
@@ -1,10 +1,1 @@
 ﻿
-using Algoritm_Practice.AlgosNDataStructures;
-using Algoritm_Practice.HackerRank;
-
-//int number = 5;
-//int result = number++ + ++number + ++number;
-
-//Console.WriteLine(result);
-//Console.WriteLine(number);
-
