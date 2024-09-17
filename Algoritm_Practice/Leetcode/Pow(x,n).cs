56509467430b2a1cf8c92baed3d2aae65576a460
Aliyavar <aliyavarguluzada@gmail.com>
2024-09-17 22:07:16 +0400

    Leetcode Pow(x, n)

---
+++ Algoritm_Practice/Leetcode/Pow(x,n).cs
@@ -0,0 +1,33 @@
+﻿namespace Algoritm_Practice.Leetcode
+{
+    public class Pow_x_n_
+    {
+        public static double Pow(double x, int n)
+        {
+
+            double result = 1;
+
+            if (n < 0)
+            {
+                int y = -1;
+                while (y >= n)
+                {
+
+                    result /= x;
+                    y--;
+                }
+            }
+            else
+            {
+                int y = 1;
+
+                while (y <= n)
+                {
+                    result *= x;
+                    y++;
+                }
+            }
+            return result;
+        }
+    }
+}
