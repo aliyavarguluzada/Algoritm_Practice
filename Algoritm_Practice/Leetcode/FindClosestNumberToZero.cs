8fe73205b21ea652bac1b475794cfd32bdd5f2ba
Aliyavar <aliyavarguluzada@gmail.com>
2024-09-19 12:51:29 +0400

    FindClosestNumberToZero Leetcode

---
+++ Algoritm_Practice/Leetcode/FindClosestNumberToZero.cs
@@ -0,0 +1,31 @@
+﻿namespace Algoritm_Practice.Leetcode
+{
+    public static class FindClosestNumberToZero
+    {
+        public static int FindClosestNumber(int[] nums)
+        {
+            //nums = [-4, -2, 1, 4, 8]
+            //nums = [-1, 1]
+
+            int closest = nums[0];
+
+            foreach (int num in nums)
+            {
+                if (Math.Abs(num) < Math.Abs(closest))
+                    closest = num;
+            }
+
+
+            if (closest < 0 && nums.Contains(Math.Abs(closest)))
+            {
+                return Math.Abs(closest);
+            }
+            else
+            {
+                return closest;
+            }
+
+        }
+
+    }
+}
--- Algoritm_Practice/Program.cs
+++ Algoritm_Practice/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using Algoritm_Practice.Leetcode;
+
+int[] nums = [-1, -2, 1, 4, 8];
+
+int a = FindClosestNumberToZero.FindClosestNumber(nums);
+
+Console.WriteLine(a);
