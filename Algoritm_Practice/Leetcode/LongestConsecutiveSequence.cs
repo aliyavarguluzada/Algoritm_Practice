539fe991681be7d11f21bc70183d2e73000c96e8
Aliyavar <aliyavarguluzada@gmail.com>
2024-10-07 16:08:35 +0400

    LongestConsecutiveSequence Leetcode

---
+++ Algoritm_Practice/Leetcode/LongestConsecutiveSequence.cs
@@ -0,0 +1,44 @@
+﻿namespace Algoritm_Practice.Leetcode
+{
+    public static class LongestConsecutiveSequence
+    {
+        public static int LongestConsecutive(int[] nums)
+        {
+            // nums = [100,4,200,1,3,2]
+            // nums = [9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6]
+
+            Array.Sort(nums);
+
+            int sequenceLength = 1;
+
+            if (nums.Length == 1)
+                return 1;
+
+            if (nums is [])
+                return 0;
+            
+            if (nums is [0, 0])
+                return 1;
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                    continue;
+
+                if (nums[i] + 1 == nums[i + 1])
+                {
+                    sequenceLength++;
+                }
+                else
+                {
+                    list.Add(sequenceLength);
+                    sequenceLength = 1;
+                }
+                list.Add(sequenceLength);
+            }
+
+            return list.Max();
+        }
+    }
+}
--- Algoritm_Practice/Program.cs
+++ Algoritm_Practice/Program.cs
@@ -1,14 +1,16 @@
 ﻿
 using Algoritm_Practice.Leetcode;
 
-int[] nums = [-1, 1, 0, -3, 3];
-int[] nums1 =
-[1, 2, 3, 4];
+int[]  nums = [9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6];
 
-int[] result = ProductOfArrayExceptSelf.ProductExceptSelf(nums1);
+//int result = LongestConsecutiveSequence.LongestConsecutive(nums);
+
+int [] nums2 = [1, 2, 0, 1];
+//int result1 = LongestConsecutiveSequence.LongestConsecutive(nums2);
 
 
-foreach (var item in result)
-{
-    Console.WriteLine(item);
-}
+int[] a = [0,0];
+int b = LongestConsecutiveSequence.LongestConsecutive(a);
+//Console.WriteLine(result);
+//Console.WriteLine(result1);
+Console.WriteLine(b);
