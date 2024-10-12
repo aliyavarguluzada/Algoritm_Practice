c88e21032326b7dc2cb8b2e9b4195135c93dd29d
Aliyavar <aliyavarguluzada@gmail.com>
2024-10-12 20:37:36 +0400

    ContainerWithMostWater Leetcode

---
+++ Algoritm_Practice/Leetcode/ContainerWithMostWater.cs
@@ -0,0 +1,27 @@
+﻿namespace Algoritm_Practice.Leetcode
+{
+    public static class ContainerWithMostWater
+    {
+        public static int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+
+            int maxArea = 0; // area = with (right - left) * height (Min(left, right))
+
+
+            while (left < right)
+            {
+                int area = Math.Min(height[left], height[right]) * (right - left);
+
+                maxArea = Math.Max(maxArea, area);
+
+                if (height[left] < height[right])
+                    left++;
+                else
+                    right--;
+            }
+            return maxArea;
+        }
+    }
+}
--- Algoritm_Practice/Program.cs
+++ Algoritm_Practice/Program.cs
@@ -1,8 +1,1 @@
 ﻿
-
-using Algoritm_Practice.Leetcode;
-
-int[] a = [2, 7, 11, 15];
-
-
-TwoSumIIInputArrayIsSorted.TwoSum(a,9);
