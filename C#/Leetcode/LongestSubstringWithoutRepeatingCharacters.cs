f26508003db4a0b8223e0d400c76c6909aca8b97
Aliyavar <aliyavarguluzada@gmail.com>
2024-11-02 21:31:11 +0400

    LongestSubstringWithoutRepeatingCharacters Leetcode

---
+++ C#/Leetcode/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,38 @@
+﻿namespace C_.Leetcode
+{
+    public static class LongestSubstringWithoutRepeatingCharacters
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            // Input: s = "abcabcbb"
+            // Output: 3
+            // Input: s = "pwwkew"
+            // Output: 3
+
+
+            int left = 0;
+            int right = 0;
+            int max = 0;
+
+            HashSet<char> chars = new HashSet<char>(26);
+
+            while (left < s.Length)
+            {
+                if (!chars.Contains(s[right]))
+                {
+                    chars.Add(s[right]);
+                    right++;
+                    max = Math.Max(chars.Count, max);
+                }
+                else
+                {
+                    chars.Remove(s[left]);
+                    left++;
+                }
+            }
+
+
+            return max;
+        }
+    }
+}
--- C#/Program.cs
+++ C#/Program.cs
@@ -2,7 +2,7 @@
 using Algoritm_Practice.Leetcode;
 using C_.Leetcode;
 
-int[] prices = [7, 1, 5, 3, 6, 4];
+string s = "pwwkew";
 
-int v = BestTimetoBuyandSellStock.MaxProfit(prices);
-Console.WriteLine(v);
+int a = LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring(s);
+Console.WriteLine(a);
